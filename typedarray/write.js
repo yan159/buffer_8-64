6f115e7354ed280dba60b0c78ca3057f895424a1
yan159 <49856914+yan159@users.noreply.github.com>
2020-09-07 10:15:21 +0300

    Update write.js

--- typedarray/write.js
+++ typedarray/write.js
@@ -36,9 +36,24 @@ function write_uint32_le(target, value, at) {
   return dv.setUint32(at + target.byteOffset, value, true)
 }
 
-function write_uint64_le(target, value, at) {
-  var dv = map.get(target);
-  return dv.setBigUint64(at + target.byteOffset, BigInt(value), true)
+const UINT_32_MAX = Math.pow(2, 32)
+function write_uint64_le(buf, num, offset) {
+    var dv = map.get(buf);
+  // var dv = map.get(target);
+  // var dv = map.get(target);
+  const top = Math.floor(num / UINT_32_MAX)
+  const rem = num - top * UINT_32_MAX
+
+  dv.setUint32(offset + buf.byteOffset, rem, true)
+  return dv.setUint32(offset + buf.byteOffset + 4, top, true)
+
+  // buf.write_uint32_le(buf, token || DEFAULT_TOKEN_VALUE, 4);
+
+  // var q = write_uint32_le(dv, rem, offset)
+  // q = write_uint32_le(q, top, offset + 4)
+  // return q;
+
+  // return dv.setBigUint64(at + target.byteOffset, BigInt(value), true)
 }
 
 function write_int16_le(target, value, at) {
